e5403e1fd36851a0a7c68bd9334558fd1a836004
José Luis Rojas Gómez <50244651+luisrojas17@users.noreply.github.com>
2025-03-05 03:57:19 -0600

    It was configured Authorization Code with PKCE. Also there were added roles, and those roles were assigned to scopes in Keycloak.

--- src/main/java/com/acme/commons/security/config/SecurityConfig.java
+++ src/main/java/com/acme/commons/security/config/SecurityConfig.java
@@ -9,10 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * This class overrides the default Spring Security configuration since the default behaviour
@@ -40,8 +41,15 @@ public class SecurityConfig {
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                authorize -> authorize.anyRequest().authenticated())
-                .oauth2Login(withDefaults())
+                authorize ->
+                        authorize.requestMatchers("/bugtracker/ui").authenticated()
+                                .requestMatchers("/bugtracker/ui/admin/**").hasAnyAuthority("SCOPE_bugtracker.admin")
+                                .requestMatchers("/bugtracker/ui/**").hasAnyAuthority("SCOPE_bugtracker.admin", "SCOPE_bugtracker.user")
+                                .anyRequest().authenticated())
+                .oauth2Login(oauth2 ->
+                        oauth2.authorizationEndpoint(
+                                cfg -> cfg.authorizationRequestResolver(
+                                        pkceResolver(clientRegistrationRepository))))
                 .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()));
 
         return http.build();
@@ -55,6 +63,36 @@ public class SecurityConfig {
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/bugtracker/ui");
 
         return oidcLogoutSuccessHandler;
+    }
+
+    /**
+     * By default, PKEC is not enabled in Spring Security. So that, if you are enabled
+     * Authorization Code with PKCE in your identity providers you can get next error:
+     *
+     * This method enables
+     * This method resolves the error:
+     * org.springframework.security.oauth2.core.OAuth2AuthenticationException:
+     * [invalid_request] Missing parameter: code_challenge_method
+     *
+     * This means that by default, Spring security does not send any code challenge
+     * and code verifier for confidential clients.
+     *
+     * See:
+     * <a href="https://www.baeldung.com/spring-security-pkce-secret-clients">
+     *     spring-security-pkce-secret-clients
+     * </a>
+     *
+     * @param repo
+     * @return
+     */
+    public OAuth2AuthorizationRequestResolver pkceResolver(ClientRegistrationRepository repo) {
+        var resolver =
+                new DefaultOAuth2AuthorizationRequestResolver(
+                        repo, "/oauth2/authorization");
+
+        resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
+
+        return resolver;
     }
 
 }
